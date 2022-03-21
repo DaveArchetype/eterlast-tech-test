f7ebc9f0a86751c314aed2748085aa64258033b8
Emanuel-David Bratasanu <davidthearchetype@gmail.com>
2022-03-21 11:19:53 +0000

    Added the Redux Toolkit store changes and the 2 actions and connected them to the store, also cleared unessential code from App.tsx, App.css. Also changed the target for the TS complier to esnext.

---
+++ src/features/nftsSlice/index.ts
@@ -0,0 +1,47 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface NFT {
+  asset_id: string;
+  name: string;
+  picture: string;
+  external_link: string;
+  description: string;
+  collection: string;
+  supply: number;
+  royalties: number;
+  date_of_creation: string;
+}
+interface NFTState {
+  creator_wallet_id: string;
+  creator_network: string;
+  assets: NFT[];
+}
+
+interface Metadata {
+  creator_wallet_id: any;
+  creator_network: any;
+}
+
+const initialState: NFTState = {
+  creator_wallet_id: "",
+  creator_network: "",
+  assets: [],
+};
+
+export const nftsSlice = createSlice({
+  name: "nfts",
+  initialState,
+  reducers: {
+    add: (state, action: PayloadAction<NFT>) => {
+      state.assets.push(action.payload);
+    },
+    setMetadata: (state, action: PayloadAction<Metadata>) => {
+      state.creator_wallet_id = action.payload.creator_wallet_id;
+      state.creator_network = action.payload.creator_network.name;
+    },
+  },
+});
+
+export const { add, setMetadata } = nftsSlice.actions;
+
+export default nftsSlice.reducer;
